16d07333dbf622b14f21278be0a3719669db3465
Leonardo Luque <64507345+LeonardoLLP@users.noreply.github.com>
2022-04-08 13:03:07 +0200

    Starting to implement get data from user function

--- main.py
+++ main.py
@@ -4,4 +4,15 @@ from pokemon_air import PokemonAir
 from pokemon_earth import PokemonEarth
 from pokemon_electricity import PokemonElectricity
 
+import pandas as pd
 
+
+
+filepath_c1 = "coach_1_pokemons.csv"
+filepath_c2 = "coach_2_pokemons.csv"
+
+c1_df = pd.DataFrame(filepath_c1, sep=",")
+c2_df = pd.DataFrame(filepath_c2, sep=",")
+
+def get_data_from_user(data: pd.DataFrame):
+    
