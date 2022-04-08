699e03a35fdc345ca55e0e0fb98ae693cd257149
Leonardo Luque <64507345+LeonardoLLP@users.noreply.github.com>
2022-04-08 11:54:34 +0200

    Actualizado metodo __init__

--- pokemon.py
+++ pokemon.py
@@ -1,5 +1,5 @@
 class Pokemon:
-    def __init__(self, id, name, moves, hp, atq, defense):
+    def __init__(self, id, name, move, hp, atq, defense):
         """Inicializa un pokemon
 
         id: int
@@ -9,23 +9,18 @@ class Pokemon:
         atq: 1 -- 10
         def: 1 -- 10
         """
-        c_moves = None
-        for move in moves:
-            if move in ["puñetazo", "patada", "codazo", "cabezazo"]:
-                continue
-            c_moves = False
-            break
-        else:
-            c_moves = True
-
+        
+        c_id = type(id) == int
+        c_name = type(name) == str
+        c_move = move in ["puñetazo", "patada", "codazo", "cabezazo"]
         c_hp = 1 <= hp <= 100
         c_atq = 1 <= atq <= 10
         c_def = 1 <= defense <= 100
 
-        if (c_moves and c_hp and c_atq and c_def):
+        if (c_id and c_name and c_move and c_hp and c_atq and c_def):
             self.id = id
             self.name = name
-            self._moves = moves
+            self._move = move
             self._hp = hp
             self._atq = atq
             self._defense = defense
@@ -33,6 +28,7 @@ class Pokemon:
             raise Exception("Values for pokemon not correct. Check docstring for more info")
 
     def print_stats(self):
-        print("Pokemon ID {} with name {} has as weapon/s") #! not finished
+        print("Pokemon ID {} with name {} has as weapon {} and health {}."
+              .format(self.id, self.name))
 
 
