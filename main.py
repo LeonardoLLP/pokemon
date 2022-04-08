d32b0dcea40725035952b362876886933f2fa2a1
Leonardo Luque <64507345+LeonardoLLP@users.noreply.github.com>
2022-04-08 12:59:04 +0200

    Updated docstring and implemented enum for mmoves

---
+++ main.py
@@ -0,0 +1,7 @@
+from pokemon import Pokemon
+from pokemon_water import PokemonWater
+from pokemon_air import PokemonAir
+from pokemon_earth import PokemonEarth
+from pokemon_electricity import PokemonElectricity
+
+
