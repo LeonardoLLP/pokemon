a9dcbadacc5138431ac5b3a35210e3f5203dc3b8
Leonardo Luque <64507345+LeonardoLLP@users.noreply.github.com>
2022-04-08 12:17:50 +0200

    Implementados algunos getters y metodo fight attack

--- pokemon.py
+++ pokemon.py
@@ -1,4 +1,7 @@
 class Pokemon:
+    pass
+
+class Pokemon:
     def __init__(self, id: int, name: str, move: str, hp: int, atq: int, defense: int):
         """Inicializa un pokemon
 
@@ -9,7 +12,7 @@ class Pokemon:
         atq: 1 -- 10
         def: 1 -- 10
         """
-        
+
         c_id = type(id) == int
         c_name = type(name) == str
         c_move = move in ["puñetazo", "patada", "codazo", "cabezazo"]
@@ -36,12 +39,30 @@ class Pokemon:
     #? No necesita getter o setter. Realmente python está programado para que ningún método sea protegido o privado. Aún así, es una buena práctica,
     #? pero este pokemon no cambia nunca su ataque o defense, no evoluciona, y el método de arriba ya hace de getter de los atributos que
     #? se espera obtener del pokemon. Sin embargo, sería conveniente asignar un método que cambiase
-    #? los puntos de vida en función del daño que reciba, útil después
+    #? los puntos de vida en función del daño que reciba, y además, otra para obtener puntos de defensa
 
     def is_hit(self, hp_to_deduct: int):
         """Deducts hp """
         self._hp -= hp_to_deduct
-    
+
+    def is_alive(self):
+        if self._hp < 1:
+            print("Pokemon is dead")
+            return False
+        else:
+            print("Pokemon is alive with {}".format(self._hp))
+            return True
+
+    def fight_attack(self, pokemon: Pokemon):
+        if pokemon.fight_defense(self._atq):
+            print("Hit {} for {} points of damage".format(pokemon.name, self._atq - pokemon._defense))
+            return True
+        else:
+            print("Attack didn't do any damage")
+            return False
+
+
+
 
 
 
